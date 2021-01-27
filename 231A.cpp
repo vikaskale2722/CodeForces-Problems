47cda3b06f0957c0edd38e487e1bd5b2c44eb339
Vikas <vk27@gmail.com>
2021-01-27 11:24:47 +0530

    Added 231A

---
+++ 231A.cpp
@@ -0,0 +1,48 @@
+// One day three best friends Petya, Vasya and Tonya decided to form a team 
+// and take part in programming contests. Participants are usually offered 
+// several problems during programming contests. Long before the start the 
+// friends decided that they will implement a problem if at least two of them 
+// are sure about the solution. Otherwise, the friends won't write the problem's 
+// solution.
+
+// This contest offers n problems to the participants. For each problem we know, 
+// which friend is sure about the solution. Help the friends find the number of 
+// problems for which they will write a solution.
+
+// Input
+// The first input line contains a single integer n (1 ≤ n ≤ 1000) — the 
+// number of problems in the contest. Then n lines contain three integers 
+// each, each integer is either 0 or 1. If the first number in the line equals 1, 
+// then Petya is sure about the problem's solution, otherwise he isn't sure. 
+// The second number shows Vasya's view on the solution, the third number 
+// shows Tonya's view. The numbers on the lines are separated by spaces.
+
+// Output
+// Print a single integer — the number of problems the friends will implement on the contest.
+
+#include<iostream>
+
+int Counter(int arr[]){
+    int count = 0;
+    for(int i=0;i<3;i++){
+        if(arr[i]==1){
+            count++;
+        }
+    }
+    return count;
+}
+
+int main(){
+    int n;
+    int finCount = 0;
+    std::cin>>n;
+    int arr[3]={};
+    for(int i=0;i<n;i++){
+        std::cin>>arr[0]>>arr[1]>>arr[2];
+        if(Counter(arr)>=2){
+            finCount++;
+        }
+    }
+    std::cout<<finCount<<std::endl;
+    return 0;
+}
