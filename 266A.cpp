3346e01e8439b0d1f0add92ae534c3d04b4b0fd6
Vikas <vk27@gmail.com>
2021-01-31 13:07:27 +0530

    Added 266A

---
+++ 266A.cpp
@@ -0,0 +1,32 @@
+// There are n stones on the table in a row, each of them can be red, green or blue. 
+// Count the minimum number of stones to take from the table so that any two neighboring 
+// stones had different colors. Stones in a row are considered neighboring if there are no 
+// other stones between them.
+
+// Input
+// The first line contains integer n (1 ≤ n ≤ 50) — the number of stones on the table.
+
+// The next line contains string s, which represents the colors of the stones. We'll 
+// consider the stones in the row numbered from 1 to n from left to right. Then the i-th 
+// character s equals "R", if the i-th stone is red, "G", if it's green and "B", if it's blue.
+
+// Output
+// Print a single integer — the answer to the problem.
+
+#include<iostream>
+
+int main(){
+    int n = 0;
+    int count = 0;
+    std::string s;
+    std::cin>>n;
+    std::cin>>s;
+
+    for(int i = 0; i < n-1; i++){
+            if(s[i] == s[i+1]){
+                count++;
+            }
+    }
+    std::cout<<count;
+    return 0;
+}
