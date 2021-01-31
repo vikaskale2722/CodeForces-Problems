f3ecec755b524926a0385fc5ba8105f5397e7b93
Vikas <vk27@gmail.com>
2021-01-31 12:50:42 +0530

    Added 281A

---
+++ 281A.cpp
@@ -0,0 +1,24 @@
+// Capitalization is writing a word with its first letter as a capital letter. 
+// Your task is to capitalize the given word.
+
+// Note, that during capitalization all the letters except the first one remains 
+// unchanged.
+
+// Input
+// A single line contains a non-empty word. This word consists of lowercase and 
+// uppercase English letters. The length of the word will not exceed 103.
+
+// Output
+// Output the given word after capitalization.
+
+
+#include<iostream>
+
+int main(){
+    std::string s;
+    std::cin>>s;
+    s[0] = toupper(s[0]);
+    std::cout<<s;
+    
+    return 0;
+}
